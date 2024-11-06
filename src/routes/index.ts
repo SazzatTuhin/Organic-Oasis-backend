9e213042dd17f32bbd3bf928ee1744ea0a7552ff
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2024-11-06 07:08:25 +0600

    server setup

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+// import UserRoutes from "../modules/user/user.route";
+
+const router: Router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+  },
+];
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,69 @@
-import express, { Application } from "express";
 import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import dotenv from "dotenv";
+import connectDB from "./config/db";
+import config from "./config/index";
+import router from "./routes";
 
-dotenv.config();
+class Server {
+  private app: Application;
+  private PORT: number | string;
 
-const app: Application = express();
-const PORT = process.env.PORT || 4000;
+  constructor() {
+    this.app = express();
 
-// Middleware setup
+    this.PORT = config.port as string;
+    this.connectDatabase();
+    this.configureMiddleware();
+    this.initializeRoutes();
+    this.handleErrors();
+  }
 
-app.use(cors()); //Enable CORS
-app.use(helmet()); // Secure HTTP headers
-app.use(morgan("combined")); // Logging HTTP requests
-app.use(express.json()); // Parse JSON bodies
+  private connectDatabase() {
+    connectDB();
+  }
 
-// Health check route
+  private configureMiddleware() {
+    this.app.use(helmet());
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(morgan("combined"));
+  }
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Server is runing healthy" });
-});
+  private initializeRoutes() {
+    this.app.use("/api/v1", router);
+    this.app.get("/api/v1/health", (req: Request, res: Response) => {
+      res.status(200).json({ message: "Server is running healthy!" });
+    });
+    this.app.use((req: Request, res: Response) => {
+      res.status(404).json({ message: "Route not found" });
+    });
+  }
 
-// Start the server
+  private handleErrors() {
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.error(err.stack);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    );
 
-app.listen(PORT, () => {
-  console.log(`Server is runing on http://localhost:${PORT}`);
-});
+    process.on("unhandledRejection", (reason: any, promise: Promise<any>) => {
+      console.error("Unhandled Rejection at:", promise, "reason:", reason);
+    });
+
+    process.on("uncaughtException", (error: Error) => {
+      console.error("Uncaught Exception:", error.message);
+    });
+  }
+
+  public start() {
+    this.app.listen(this.PORT, () => {
+      console.log(`Server is running on http://localhost:${this.PORT}`);
+    });
+  }
+}
+
+const server = new Server();
+server.start();
